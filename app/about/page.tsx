4952581c3311a36707c2404b3915aba77b958dcc
afkshreshtha <shreshtha1324@gmail.com>
2025-04-27 23:44:25 +0530

    fixabout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -189,7 +189,7 @@ export default function AboutPage() {
                 width="100%" 
                 height="100%" 
                 style={{ border: 0 }} 
-                allowFullScreen="" 
+                allowFullScreen
                 loading="lazy" 
                 referrerPolicy="no-referrer-when-downgrade"
                 className="absolute inset-0"
